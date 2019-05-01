fe26dbfee88adf1f20c03e8bd8450770afbfdd0e
Tanuka Das <das.tanuka@yahoo.com>
2019-05-01 05:37:51 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,6 @@
 def my_select(collection)
  # your code here!
+ collection.select {|num| num.even? }
 end
+#What is the return value of yielding a character to a block?
+#How can you ensure your .select method can respond appropriately to being passed an argument of an empty collection?
